f9f780e20922cab4c0dc8b0cacb78d8e5961ae0c
Haeseok Kang <hotbloodturtle@gmail.com>
2023-02-20 18:37:31 +0900

    add - typeorm migration cli

---
+++ src/database/data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "next_typeorm",
+    entities: ["src/database/entities/*.ts"],
+    migrations: ["src/database/migrations/*.ts"],
+    migrationsTableName: 'migrations',
+    logging: true,
+    synchronize: false,
+})
